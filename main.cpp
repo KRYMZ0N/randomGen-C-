4f9c63062d6c7a4ad8c554e1c9f116b51e52be55
KRYMZ0N <jackgrimes05@yahoo.com>
2022-08-15 04:44:33 -0400

    init commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool loop = true;
+    string s;
+
+
+    cout << "hello cunt!\n";
+
+    while(loop) {
+        
+        cin >> s;
+
+        if (s == "poop") {
+            cout << "pee";
+        }
+    }
+
+    return 0;
+}
